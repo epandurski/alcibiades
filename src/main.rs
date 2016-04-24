b79b692a702d66abd2b3451fdcb1d8b8615918de
Evgeni Pandurski <epandurski@gmail.com>
2016-04-25 02:46:57 +0300

    Implement generation of blockers and beyond array.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod utils;
 
 fn main() {
-    let attack_sets = utils::generate_attack_sets();
+    let attack_sets = utils::generate_attack_and_blockers_arrays();
     println!("Hello, world!");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -44,16 +44,16 @@ pub struct Board {
 //     pub fn from_fen(f: &str) -> Board {}
 // }
 
-pub type AttackSets = [[u64; 64]; 5];  // for example
+pub type AttackArray = [[u64; 64]; 5];  // for example
                                    // attack_set[Pieces::Queen][0]
                                    // gives the attack set for a queen
                                    // at A1.
 
 
-pub fn generate_attack_sets() -> AttackSets {
+pub fn generate_attack_and_blockers_arrays() -> (AttackArray, AttackArray) {
 
     fn square2grid_index(i: usize) -> usize {
-        ((i / 8) * 10 + (i % 8) + 21) as usize
+        ((i / 8) * 10 + (i % 8) + 21)
     }
     
     // Generate a 10x12 grid -- 0xff is the "out of board" marker.
@@ -70,34 +70,43 @@ pub fn generate_attack_sets() -> AttackSets {
                                          [-11, 0, -9, 0, 0, 9, 0, 11],
                                          [-21, -19, -12, -8, 8, 12, 19, 21]];
     
-    // At the end will hold the attack bitset for each piece on each
-    // possible square.
-    let mut attack_sets = [[0u64; 64]; 5];
+    // At the end those arrays will hold the attack bitsets, and the
+    // "blockers and beyond" bitsets for each piece on each possible
+    // square.
+    let mut attack_array = [[0u64; 64]; 5];
+    let mut blockers_array = [[0u64; 64]; 5];
     
     for piece_type in 0..5 {
         for square in 0..64 {
-            let mut bitset = 0u64;
+            let mut attack = 0u64;
+            let mut blockers = 0u64;
             for move_direction in 0..8 {
                 let delta = PIECE_DELTAS[piece_type][move_direction];
                 if delta != 0 {
+                    let mut last_mask = 0u64;
                     let mut curr_grid_index = square2grid_index(square);
                     loop {
                         curr_grid_index = (curr_grid_index as i8 + delta) as usize;
                         let curr_square = grid[curr_grid_index];
                         if curr_square != 0xff {
-                            bitset |= 1 << curr_square;
+                            last_mask = 1 << curr_square;
+                            attack |= last_mask;
+                            blockers |= last_mask;
                             if PIECE_LONGRANGE[piece_type] {
                                 continue;
                             }
-                        };
+                        }
+                        blockers &= !last_mask;
                         break;
                     }
                 }
             }
-            attack_sets[piece_type][square] = bitset;
+            attack_array[piece_type][square] = attack;
+            blockers_array[piece_type][square] = blockers;
         }
     }
-    attack_sets
+    
+    (attack_array, blockers_array)
 }
 
 // Forsyth–Edwards Notation
