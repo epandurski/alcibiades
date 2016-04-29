475f97cb7ae2896b1b76239cc0804b3360159a88
Evgeni Pandurski <epandurski@gmail.com>
2016-04-29 16:48:44 +0300

    Added bitscan_and_clear().

--- src/bitsets.rs
+++ src/bitsets.rs
@@ -45,16 +45,23 @@ pub fn smeared_ls1b_down(x: u64) -> u64 {
     x | (x - 1)
 }
 
-
+const DEBRUIJN64: Wrapping<u64> = Wrapping(0x03f79d71b4cb0a89);
+static INDEX64: [Square; 64] = [0, 1, 48, 2, 57, 49, 28, 3, 61, 58, 50, 42, 38, 29, 17, 4, 62,
+                                55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5, 63,
+                                47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11,
+                                46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6];
 #[inline]
 pub fn bitscan_forward(b: u64) -> Square {
-    const DEBRUIJN64: Wrapping<u64> = Wrapping(0x03f79d71b4cb0a89);
-    static INDEX64: [Square; 64] = [0, 1, 48, 2, 57, 49, 28, 3, 61, 58, 50, 42, 38, 29, 17, 4, 62,
-                                    55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5, 63,
-                                    47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11,
-                                    46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6];
     assert!(b != 0);
     INDEX64[((Wrapping(ls1b(b)) * DEBRUIJN64).0 >> 58) as usize]
+}
+
+#[inline]
+pub fn bitscan_and_clear(b: &mut u64) -> Square {
+    assert!(*b != 0);
+    let ls1b = ls1b(*b);
+    *b &= !ls1b;
+    INDEX64[((Wrapping(ls1b) * DEBRUIJN64).0 >> 58) as usize]
 }
 
 
@@ -89,6 +96,9 @@ mod tests {
         assert_eq!(bitscan_forward(0b1001101), 0);
         assert_eq!(bitscan_forward(0b1001000), 3);
         assert_eq!(bitscan_forward(0xf000000000000000), 60);
+        let mut b = 0b1100100;
+        assert_eq!(bitscan_and_clear(&mut b), 2);
+        assert_eq!(b, 0b1100000);
     }
 
 }
