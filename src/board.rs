d6818eb321295341bb83f2a136f98e3faa82594f
Evgeni Pandurski <epandurski@gmail.com>
2016-05-06 19:20:44 +0300

    Added failing test.

--- src/board.rs
+++ src/board.rs
@@ -1009,4 +1009,18 @@ mod tests {
                    0);
     }
 
+    #[test]
+    fn test_move_generation_3() {
+        use basetypes::*;
+        let mut piece_type = [0u64; 6];
+        let mut color = [0u64; 2];
+        piece_type[KING] |= 1 << H1; color[WHITE] |= 1 << H1;
+        piece_type[PAWN] |= 1 << G4; color[WHITE] |= 1 << G4;
+        piece_type[ROOK] |= 1 << E4; color[WHITE] |= 1 << E4;
+        piece_type[KING] |= 1 << H4; color[BLACK] |= 1 << H4;
+        piece_type[PAWN] |= 1 << F4; color[BLACK] |= 1 << F4;
+        let b = Board::new(&piece_type, &color);
+        assert_eq!(b.generate_moves(BLACK, H4, 0, 0, 1 << G3, &mut MoveStack::new()),
+                   1);
+    }
 }
