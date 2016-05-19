63ce6ce3bdfd914b108af7b312c236d866d291bc
Evgeni Pandurski <epandurski@gmail.com>
2016-05-19 15:23:02 +0300

    refactor obstacles()

--- src/position/castling_rights.rs
+++ src/position/castling_rights.rs
@@ -75,21 +75,14 @@ impl CastlingRights {
     // be possible.
     #[inline(always)]
     pub fn obstacles(&self, color: Color, side: CastlingSide) -> u64 {
+        const OBSTACLES: [[u64; 2]; 2] = [[1 << B1 | 1 << C1 | 1 << D1, 1 << F1 | 1 << G1],
+                                          [1 << B8 | 1 << C8 | 1 << D8, 1 << F8 | 1 << G8]];
         if self.can_castle(color, side) {
-            let obstacles = if side == QUEENSIDE {
-                1 << B1 | 1 << C1 | 1 << D1
-            } else {
-                1 << F1 | 1 << G1
-            };
-            if color == WHITE {
-                obstacles
-            } else {
-                obstacles << 56
-            }
+            OBSTACLES[color][side]
         } else {
             // Castling is not allowed, therefore every piece on every
             // square on the board can be considered an obstacle.
-            return UNIVERSAL_SET;
+            UNIVERSAL_SET
         }
     }
 
@@ -113,6 +106,11 @@ impl CastlingRights {
     pub fn set_for(&mut self, color: Color, rights: usize) {
         assert!(color <= 1);
         assert!(rights <= 0b11);
+
+        // Since the raw value of "CastlingRights" frequently is used
+        // as an array index without boundary checking, we guarantee
+        // that the raw value is always less than 16 by sanitizing the
+        // passed "rights".
         self.0 = if color == WHITE {
             self.0 & 0b1100 | rights & 0b0011
         } else {
