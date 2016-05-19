30bf8dd7bbf638a729f4405c767c1931e46b704c
Evgeni Pandurski <epandurski@gmail.com>
2016-05-19 21:03:05 +0300

    moved comment

--- src/position/board.rs
+++ src/position/board.rs
@@ -39,11 +39,46 @@ pub struct Board {
 
 
 impl Board {
-    // Create a new board instance from a FEN (Forsyth–Edwards
-    // Notation) string.
+    // Create a new board instance from a FEN string.
     //
-    // It makes expensive verification to make sure that the board is
-    // legal.
+    // A FEN (Forsyth–Edwards Notation) string defines a particular
+    // position using only the ASCII character set. A FEN string
+    // contains six fields separated by a space. The fields are:
+    //
+    // 1) Piece placement (from white's perspective). Each rank is
+    //    described, starting with rank 8 and ending with rank
+    //    1. Within each rank, the contents of each square are
+    //    described from file A through file H. Following the Standard
+    //    Algebraic Notation (SAN), each piece is identified by a
+    //    single letter taken from the standard English names. White
+    //    pieces are designated using upper-case letters ("PNBRQK")
+    //    whilst Black uses lowercase ("pnbrqk"). Blank squares are
+    //    noted using digits 1 through 8 (the number of blank
+    //    squares), and "/" separates ranks.
+    //
+    // 2) Active color. "w" means white moves next, "b" means black.
+    //
+    // 3) Castling availability. If neither side can castle, this is
+    //    "-". Otherwise, this has one or more letters: "K" (White can
+    //    castle kingside), "Q" (White can castle queenside), "k"
+    //    (Black can castle kingside), and/or "q" (Black can castle
+    //    queenside).
+    //
+    // 4) En passant target square (in algebraic notation). If there's
+    //    no en passant target square, this is "-". If a pawn has just
+    //    made a 2-square move, this is the position "behind" the
+    //    pawn. This is recorded regardless of whether there is a pawn
+    //    in position to make an en passant capture.
+    //
+    // 5) Halfmove clock. This is the number of halfmoves since the
+    //    last pawn advance or capture. This is used to determine if a
+    //    draw can be claimed under the fifty-move rule.
+    //
+    // 6) Fullmove number. The number of the full move. It starts at
+    //    1, and is incremented after Black's move.
+    //
+    // This function makes expensive verification to make sure that
+    // the board is legal.
     pub fn from_fen(fen: &str) -> Result<Board, IllegalBoard> {
 
         // separate the FEN string parts
@@ -87,7 +122,7 @@ impl Board {
             castling: castling,
             to_move: to_move,
             occupied: placement.color[WHITE] | placement.color[BLACK],
-            hash: 0,
+            hash: Default::default(),
             _checkers: Cell::new(UNIVERSAL_SET),
             _king_square: Cell::new(64),
         };
@@ -108,7 +143,7 @@ impl Board {
         self.geometry
     }
 
-    
+
     // Return an array of 6 occupation bitboards -- one for each piece
     // type.
     #[inline]
--- src/position/mod.rs
+++ src/position/mod.rs
@@ -24,39 +24,6 @@ pub struct Position {
 }
 
 impl Position {
-    // A FEN (Forsyth–Edwards Notation) string defines a particular
-    // position using only the ASCII character set.
-    //
-    // A FEN string contains six fields separated by a space. The fields are:
-    //
-    // 1) Piece placement (from white's perspective). Each rank is described, starting
-    //    with rank 8 and ending with rank 1. Within each rank, the contents of each
-    //    square are described from file A through file H. Following the Standard
-    //    Algebraic Notation (SAN), each piece is identified by a single letter taken
-    //    from the standard English names. White pieces are designated using upper-case
-    //    letters ("PNBRQK") whilst Black uses lowercase ("pnbrqk"). Blank squares are
-    //    noted using digits 1 through 8 (the number of blank squares), and "/"
-    //    separates ranks.
-    //
-    // 2) Active color. "w" means white moves next, "b" means black.
-    //
-    // 3) Castling availability. If neither side can castle, this is "-". Otherwise,
-    //    this has one or more letters: "K" (White can castle kingside), "Q" (White
-    //    can castle queenside), "k" (Black can castle kingside), and/or "q" (Black
-    //    can castle queenside).
-    //
-    // 4) En passant target square (in algebraic notation). If there's no en passant
-    //    target square, this is "-". If a pawn has just made a 2-square move, this
-    //    is the position "behind" the pawn. This is recorded regardless of whether
-    //    there is a pawn in position to make an en passant capture.
-    //
-    // 5) Halfmove clock. This is the number of halfmoves since the last pawn advance
-    //    or capture. This is used to determine if a draw can be claimed under the
-    //    fifty-move rule.
-    //
-    // 6) Fullmove number. The number of the full move. It starts at 1, and is
-    //    incremented after Black's move.
-    //
     pub fn from_fen(fen: &str) -> Result<Position, IllegalBoard> {
         let parts: Vec<_> = fen.split_whitespace().collect();
 
