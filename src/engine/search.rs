880f260a810252025135cfaada5d501da41a1085
Evgeni Pandurski <epandurski@gmail.com>
2016-06-18 18:42:44 +0300

    implement dummy search

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -1,11 +1,45 @@
 use basetypes::*;
+use chess_move::MoveStack;
+use tt::*;
 use position::{Position, Value};
 
 
-pub fn search(p: Position,
-              alpha: Value, // lower bound
+pub fn search(tt: &TranspositionTable,
+              p: &mut Position,
+              mut alpha: Value, // lower bound
               beta: Value, // upper bound
               depth: usize,
-              send_node_count: &Fn(NodeCount) -> bool) {
+              node_count: &mut NodeCount,
+              report_node_count: &Fn(NodeCount) -> bool)
+              -> Value {
+    if depth == 0 {
+        let (value, nodes) = p.evaluate_quiescence(alpha, beta);
+        *node_count += nodes;
+        value
+    } else {
+        // TODO: use something faster than `Vec`.
+        let mut move_stack = MoveStack::new();
+        move_stack.save();
+        p.generate_moves(&mut move_stack);
+        while let Some(next_move) = move_stack.remove_best_move() {
+            if p.do_move(next_move) {
+                *node_count += 1;
+                let value = -search(tt,
+                                    p,
+                                    -beta,
+                                    -alpha,
+                                    depth - 1,
+                                    node_count,
+                                    report_node_count);
+                p.undo_move();
+                if value >= beta {
+                    alpha = value;
+                    break;
+                }
+            }
 
+        }
+        move_stack.restore();
+        alpha
+    }
 }
