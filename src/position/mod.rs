9e4b351637c2762a868637d2ec3b0db5ccb27e8f
Evgeni Pandurski <epandurski@gmail.com>
2016-06-21 15:56:46 +0300

    add halfmove_clock guard in evaluate_static

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -154,23 +154,23 @@ impl Position {
     #[inline]
     pub fn evaluate_static(&self, lower_bound: Value, upper_bound: Value) -> Value {
         assert!(lower_bound <= upper_bound);
-        // TODO: Implement a real evaluation. Also, this evaluation
-        // probably belongs to `Board`, because in order for the
-        // search to provably be stable positions with the same hash,
-        // should return the same static evaluation.
-
-        let board = self.board();
-        let piece_type = board.piece_type();
-        let color = board.color();
-        let us = board.to_move();
-        let them = 1 ^ us;
-        let mut result = 0;
-        for piece in QUEEN..NO_PIECE {
-            result += PIECE_VALUES[piece] *
-                      (pop_count(piece_type[piece] & color[us]) as i16 -
-                       pop_count(piece_type[piece] & color[them]) as i16);
+        if self.state().halfmove_clock < 100 {
+            // TODO: Implement a real evaluation.
+            let board = self.board();
+            let piece_type = board.piece_type();
+            let color = board.color();
+            let us = board.to_move();
+            let them = 1 ^ us;
+            let mut result = 0;
+            for piece in QUEEN..NO_PIECE {
+                result += PIECE_VALUES[piece] *
+                          (pop_count(piece_type[piece] & color[us]) as i16 -
+                           pop_count(piece_type[piece] & color[them]) as i16);
+            }
+            result
+        } else {
+            0
         }
-        result
     }
 
     /// Performs "quiescence search" and returns an evaluation.
