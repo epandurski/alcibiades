961d46095a1da8f277e469390683d55fac7e81a3
Evgeni Pandurski <epandurski@gmail.com>
2016-07-18 15:13:40 +0300

    comments

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -220,10 +220,10 @@ impl UciEngine for Engine {
         while let Ok(report) = self.reports.try_recv() {
             if self.is_thinking {
                 match report {
+                    // Search progress.
                     search::Report::Progress { search_id, searched_nodes, depth }
                         if search_id == self.search_id => {
 
-                        // Report search progress.
                         let thinking_duration = self.thinking_since.elapsed().unwrap();
                         self.searched_nodes = searched_nodes;
                         self.searched_time = 1000 * thinking_duration.as_secs() +
@@ -236,8 +236,9 @@ impl UciEngine for Engine {
                             self.report_progress();
                         }
                     }
+
+                    // Terminate the search (unless pondering of infinite).
                     search::Report::Done { search_id, .. } if search_id == self.search_id => {
-                        // Terminate the search if not pondering of infinite.
                         if !self.is_pondering {
                             self.stop_when = if let TimeManagement::Infinite = self.stop_when {
                                 TimeManagement::Infinite
