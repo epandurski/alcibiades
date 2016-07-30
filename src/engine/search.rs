e29c96c447149c13acb62897a85a69b9247ae7a7
Evgeni Pandurski <epandurski@gmail.com>
2016-07-30 22:18:09 +0300

    more "softness"

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -266,10 +266,9 @@ impl<'a> Search<'a> {
                 let value = -try!(self.run(-beta, -alpha, max(0, reduced_depth - 1) as u8, false));
                 self.position.undo_move();
                 if value >= beta {
-                    // TODO: What should we store here?
                     self.tt.store(hash,
-                                  EntryData::new(beta, BOUND_LOWER, depth, 0, eval_value));
-                    return Ok(Some(beta));
+                                  EntryData::new(value, BOUND_LOWER, depth, 0, eval_value));
+                    return Ok(Some(value));
                 }
             }
         }
