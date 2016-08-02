214a7ce3465d90a7e15cdcbbdc7b764f0818f1f6
Evgeni Pandurski <epandurski@gmail.com>
2016-08-03 01:42:09 +0300

    Do not check is_repeated() in evaluate_static()

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -233,24 +233,20 @@ impl Position {
     /// `-19999` and `19999`.
     #[inline]
     pub fn evaluate_static(&self) -> Value {
-        if self.is_repeated() {
-            0
-        } else {
-            evaluate_board(self.board())
-        }
+        evaluate_board(self.board())
     }
 
     /// Performs a "quiescence search" and returns an evaluation.
     ///
-    /// The "quiescence search" is a rathrer restricted tree search
-    /// which consisers only a limited set of moves (for example:
-    /// winning captures, pawn promotions to queen, check
-    /// evasions). The goal is to statically evaluate only "quiet"
-    /// positions (positions where there are no winning tactical moves
-    /// to be made). Although this search can cheaply and correctly
-    /// resolve many tactical issues, it is particularly blind to
-    /// other simple tactical threads like most kinds of forks,
-    /// checks, even a checkmate in one move.
+    /// The "quiescence search" is a restricted tree search which
+    /// considers only a limited set of moves (for example: winning
+    /// captures, pawn promotions to queen, check evasions). The goal
+    /// is to statically evaluate only "quiet" positions (positions
+    /// where there are no winning tactical moves to be
+    /// made). Although this search can cheaply and correctly resolve
+    /// many tactical issues, it is blind to other simple tactical
+    /// threads like most kinds of forks, checks, even a checkmate in
+    /// one move.
     /// 
     /// `lower_bound` and `upper_bound` together give the interval
     /// within which an as precise as possible evaluation is
