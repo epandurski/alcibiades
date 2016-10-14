cf02b890829d829f79ddd942096be9bdf88eec4f
Evgeni Pandurski <epandurski@gmail.com>
2016-10-14 23:32:35 +0300

    more work on AspiratedSearch

--- src/search/threading.rs
+++ src/search/threading.rs
@@ -421,6 +421,14 @@ fn serve<T: SearchRefinement>(tt: Arc<Tt>, commands: Receiver<Command>, reports:
 }
 
 
+/// Aspiration windows are a way to reduce the search space in the
+/// search. We use the value from the last iteration and calculate a
+/// window around this as the alpha-beta bounds. Because the window is
+/// narrower, more beta cutoffs are achieved, and the search takes a
+/// shorter time. The drawback is that if the true score is outside
+/// this window, then a costly re-search must be made. But then most
+/// probably the re-search will be much faster, because many positions
+/// will be remembered from the TT.
 struct AspirationSearch {
     slave_commands_tx: Sender<Command>,
     slave_reports_rx: Receiver<Report>,
@@ -461,13 +469,30 @@ impl AspirationSearch {
             .unwrap();
     }
 
-    /// A helper method. It increases the half-width of the aspiration
-    /// window.
+    /// A helper method. It increases the value with which the
+    /// aspiration window will be widened if the next search fails.
     fn increase_delta(&mut self) {
         self.delta += 3 * self.delta / 8;
         if self.delta > 1500 {
             self.delta = 1_000_000;
         }
+    }
+
+    /// A helper method. It widens the aspiration window if necessary.
+    fn update_aspiration_window(&mut self) -> bool {
+        let v = self.value as isize;
+        if self.value <= self.alpha && self.lower_bound < self.alpha {
+            // Set smaller alpha.
+            self.alpha = max(v - self.delta, self.lower_bound as isize) as Value;
+            self.increase_delta();
+            return true;
+        } else if self.value >= self.beta && self.upper_bound > self.beta {
+            // Set bigger beta.
+            self.beta = min(v + self.delta, self.upper_bound as isize) as Value;
+            self.increase_delta();
+            return true;
+        }
+        false
     }
 }
 
@@ -487,14 +512,14 @@ impl SearchRefinement for AspirationSearch {
             reports: reports,
             search_id: 0,
             position: Position::from_fen(::STARTING_POSITION).ok().unwrap(),
-            depth: 0,
-            lower_bound: 0,
-            upper_bound: 1,
-            value: 0,
             searched_nodes: 0,
-            delta: 1,
-            alpha: 0,
-            beta: 1,
+            depth: 0,
+            lower_bound: -29999,
+            upper_bound: 29999,
+            value: VALUE_UNKNOWN,
+            delta: INITIAL_ASPIRATION_WINDOW as isize,
+            alpha: -29999,
+            beta: 29999,
         }
     }
 
@@ -508,100 +533,56 @@ impl SearchRefinement for AspirationSearch {
         debug_assert!(lower_bound < upper_bound);
         self.search_id = search_id;
         self.position = position;
+        self.searched_nodes = 0;
         self.depth = depth;
         self.lower_bound = lower_bound;
         self.upper_bound = upper_bound;
         self.value = value;
         self.delta = INITIAL_ASPIRATION_WINDOW as isize;
 
-        // Set the initial aspirated bounds (`self.alpha` and
-        // `self.beta`).
-        let (a, b) = if self.depth < 5 || self.value == VALUE_UNKNOWN {
-            (self.lower_bound, self.upper_bound)
+        // Set the initial aspiration window (`self.alpha`, `self.beta`).
+        let (a, b) = if value == VALUE_UNKNOWN || value < lower_bound || value > upper_bound {
+            (lower_bound, upper_bound)
         } else {
-            let v = self.value as isize;
-            (max(v - self.delta, self.lower_bound as isize) as Value,
-             min(v + self.delta, self.upper_bound as isize) as Value)
+            let v = value as isize;
+            (max(v - self.delta, lower_bound as isize) as Value,
+             min(v + self.delta, upper_bound as isize) as Value)
         };
-        if a < b {
-            self.alpha = a;
-            self.beta = b;
-        } else {
-            // This may happen if (v - delta, v + delta) and
-            // (lower_bound, upper_bound) do not intersect.
-            self.alpha = self.lower_bound;
-            self.beta = self.upper_bound;
-        }
+        self.alpha = a;
+        self.beta = b;
 
         self.start_aspirated_search();
     }
 
     fn progress(&mut self, search_is_terminated: bool) -> bool {
-        // First we set up the aspiration window. Aspiration windows are a way
-        // to reduce the search space in the search. We use `current_value`
-        // from the last iteration of `depth`, and calculate a window around
-        // this as the alpha-beta bounds. Because the window is narrower, more
-        // beta cutoffs are achieved, and the search takes a shorter time. The
-        // drawback is that if the true score is outside this window, then a
-        // costly re-search must be made. But then most probably the re-search
-        // will be much faster, because many positions will be remembered from
-        // the TT.
-
-        let Report { searched_nodes, value, done, .. } = self.slave_reports_rx.recv().unwrap();
+        let Report { searched_nodes, depth, value, done, .. } = self.slave_reports_rx
+                                                                    .recv()
+                                                                    .unwrap();
+        let searched_nodes = self.searched_nodes + searched_nodes;
+        let depth = if done && !search_is_terminated {
+            self.searched_nodes = searched_nodes;
+            self.value = value;
+            if self.update_aspiration_window() {
+                // `value` is outside the aspiration window and we
+                // must start another search.
+                self.start_aspirated_search();
+                return false;
+            }
+            depth
+        } else {
+            0
+        };
 
         self.reports
             .send(Report {
                 search_id: self.search_id,
-                searched_nodes: self.searched_nodes + searched_nodes,
-                depth: 0,
+                searched_nodes: searched_nodes,
+                depth: depth,
                 value: self.value,
-                done: false,
+                done: done,
             })
             .ok();
 
-        if done {
-            self.searched_nodes += searched_nodes;
-            if !search_is_terminated {
-                self.value = value;
-
-                // Check if `value` is within the aspiration window
-                // (alpha, beta). If not so, we must consider running a
-                // re-search.
-                let v = value as isize;
-                if value <= self.alpha && self.lower_bound < self.alpha {
-                    // Set smaller `self.alpha`.
-                    self.alpha = max(v - self.delta, self.lower_bound as isize) as Value;
-                    self.increase_delta();
-                    self.start_aspirated_search();
-                    return false;
-                } else if self.value >= self.beta && self.upper_bound > self.beta {
-                    // Set bigger `self.beta`.
-                    self.beta = min(v + self.delta, self.upper_bound as isize) as Value;
-                    self.increase_delta();
-                    self.start_aspirated_search();
-                    return false;
-                }
-            }
-
-            // The search is done -- send a final report.
-            self.reports
-                .send(Report {
-                    search_id: self.search_id,
-                    searched_nodes: self.searched_nodes,
-                    depth: if search_is_terminated {
-                        0
-                    } else {
-                        self.depth
-                    },
-                    value: self.value,
-                    done: true,
-                })
-                .ok();
-            true
-
-        } else {
-            // The search is not done yet.
-            false
-        }
+        done
     }
 }
