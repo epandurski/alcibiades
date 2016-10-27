8d541d16d198209d788f8618ac6c856295c96c38
Evgeni Pandurski <epandurski@gmail.com>
2016-10-27 20:46:37 +0300

    use MultipvSearcher

--- src/utils.rs
+++ src/utils.rs
@@ -60,7 +60,7 @@ pub struct SearchStatus {
 pub struct SearchThread {
     tt: Arc<Tt>,
     status: SearchStatus,
-    searcher: DeepeningSearcher<AspirationSearcher<AlphabetaSearcher>>,
+    searcher: DeepeningSearcher<MultipvSearcher<AlphabetaSearcher>>,
 }
 
 
