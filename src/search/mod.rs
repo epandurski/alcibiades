d2130d248bed4d508bba3bb700a5085851c5ad42
Evgeni Pandurski <epandurski@gmail.com>
2016-11-14 19:53:53 +0200

    remove blank line

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -187,7 +187,6 @@ pub trait SearchExecutor {
 /// **Important note:** Repeating positions are considered a draw
 /// after the first repetition, not after the second one as the chess
 /// rules prescribe. This is done in the sake of efficiency.
-
 pub trait SearchNode: Send {
     /// Returns an almost unique hash value for the position.
     ///
