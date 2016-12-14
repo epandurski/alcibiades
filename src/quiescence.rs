fdfbba32e34beafac7f4c424073d205999ccb257
Evgeni Pandurski <epandurski@gmail.com>
2016-12-15 00:18:18 +0200

    add QsearchParams struct

--- src/quiescence.rs
+++ src/quiescence.rs
@@ -2,23 +2,62 @@ use chesstypes::*;
 use board::MoveGenerator;
 
 
-pub struct QsearchResult<T> {
-    value: Value,
-    searched_nodes: u64,
-    hints: T,
+/// Parameters describing a quiescence search.
+pub struct QsearchParams<T: MoveGenerator> {
+    /// The root position for the search.
+    pub position: T,
+
+    /// The number of half-moves since the last piece capture or pawn
+    /// advance.
+    pub halfmove_clock: u8,
+
+    /// The requested search depth.
+    ///
+    /// This is the depth at which the main search stops and the
+    /// quiescence search takes on. It should be between `DEPTH_MIN`
+    /// and `DEPTH_MAX`. Normally, it will be zero or less. The
+    /// quiescence search implementation may decide to perform less
+    /// thorough analysis when `depth` is smaller.
+    pub depth: Depth,
+
+    /// The lower bound for the new search.
+    ///
+    /// Should be no lesser than `VALUE_MIN`.
+    pub lower_bound: Value,
+
+    /// The upper bound for the new search.
+    ///
+    /// Should be greater than `lower_bound`, but no greater than
+    /// `VALUE_MAX`.
+    pub upper_bound: Value,
+
+    /// Position's static evaluation, or `VALUE_UNKNOWN`.
+    ///
+    /// Saves the re-calculation if position's static evaluation is
+    /// already available.
+    pub static_evaluation: Value,
 }
+
+
+/// Results from a quiescence search.
+pub struct QsearchResult<T> {
+    /// The calculated evaluation for the analyzed position.
+    pub value: Value,
+
+    /// The number of positions that were searched in order to
+    /// calculate the evaluation.
+    pub searched_nodes: u64,
+
+    /// Auxiliary hints regarding the analyzed position.
+    pub hints: T,
+}
+
 
 /// A trait used to perform quiescence searches.
 pub trait Qsearch {
-    type Hints;
     type MoveGenerator: MoveGenerator;
+    type Hints;
 
     /// TODO
-    fn qsearch(position: Self::MoveGenerator,
-               halfmove_clock: u8,
-               depth: Depth,
-               lower_bound: Value,
-               upper_bound: Value,
-               static_evaluation: Value)
-               -> QsearchResult<Self::Hints>;
+    fn qsearch(params: QsearchParams<Self::MoveGenerator>) -> QsearchResult<Self::Hints>;
 }
