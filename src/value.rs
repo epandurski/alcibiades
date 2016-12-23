fa71cac8b90abebcc7ca27d8148f76710e6344a9
Evgeni Pandurski <epandurski@gmail.com>
2016-12-23 22:38:48 +0200

    docs

--- src/value.rs
+++ src/value.rs
@@ -13,15 +13,31 @@
 ///
 /// * `VALUE_UNKNOWN` has the special meaning of "unknown value".
 ///
-/// * `VALUE_MAX` designates a checkmate (a win).
-///
-/// * `VALUE_MIN` designates a checkmate (a loss).
-///
 /// * Values bigger than `VALUE_EVAL_MAX` designate a win by
 ///   inevitable checkmate.
 ///
 /// * Values smaller than `VALUE_EVAL_MIN` designate a loss by
 ///   inevitable checkmate.
+///
+/// * `VALUE_MAX` designates a checkmate (a win).
+///
+///    * `VALUE_MAX - 1` designates an inevitable checkmate (a win) in
+///       1 half-move.
+///
+///    * `VALUE_MAX - 2` designates an inevitable checkmate (a win) in
+///      2 half-moves.
+///
+///    * and so forth.
+///
+/// * `VALUE_MIN` designates a checkmate (a loss).
+///
+///    * `VALUE_MIN + 1` designates an inevitable checkmate (a loss)
+///      in 1 half-move.
+///
+///    * `VALUE_MIN + 2` designates an inevitable checkmate (a loss)
+///      in 2 half-moves.
+///
+///    * and so forth.
 pub type Value = i16;
 
 pub const VALUE_UNKNOWN: Value = VALUE_MIN - 1;
