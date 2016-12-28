1df04fa4417d60f2af7e03453785c19cc7eb6106
Evgeni Pandurski <epandurski@gmail.com>
2016-12-28 15:38:03 +0200

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,16 @@
 //!
 //! # Why a framework?
 //!
-//! There is lots of knowledge about how to write a chess engine out
-//! there, and a lot of place for innovation too. Writing a chess
-//! engine is fun, but even for the simplest engine there is a lot of
-//! complex (and boring) things that have to be implemented correctly:
-//! the UCI protocol communication, the rules, the static exchange
-//! evaluation, and so forth. Thousands of programmers had been
-//! implementing those things over and over again.
+//! There is lots of knowledge out there about how to write a chess
+//! engine, and there is a lot of place for innovation too. Writing a
+//! chess engine is fun, but even for the simplest engine there is a
+//! lot of complex (and boring) things that have to be implemented
+//! correctly: the UCI protocol communication, the rules, the static
+//! exchange evaluation, and so forth. Thousands of programmers had
+//! been re-implementing those things over and over again.
 //!
-//! So, if you want to write your own chess engine, you face two
-//! unpleasant choices: You either roll up your sleeves and implement
+//! So, if you want to write your own chess engine, you face an
+//! unpleasant choice: You either roll up your sleeves and implement
 //! all the hard stuff from scratch, or you take someone else's chess
 //! engine and struggle to understand its cryptic, undocumented source
 //! code, hoping that it is general enough to allow modification. This
@@ -24,7 +24,7 @@
 //!
 //! # Usage
 //!
-//! Here is how simple it is to create a chess engine using this
+//! Here is how simple it is to create a chess engine using the
 //! framework:
 //!
 //! ```rust,no_run
@@ -43,7 +43,18 @@
 //!     })
 //! }
 //! ```
-
+//!
+//! This engine is all assembled from the "in stock" implementations
+//! of the different framework traits.
+//!
+//! In reality, you will probably want to write your own
+//! implementations of at least some of the framework traits. While
+//! doing so, you are not limited to implementing only the methods
+//! required by the traits. You can add your own methods and use
+//! Rust's incredible capabilities for generic programming to call
+//! them. For example you may write your own static position evaluator
+//! which have the `consult_endgame_table()` method. Then you will be
+//! able to write search algorithm that makes use of this method.
 
 #[macro_use]
 extern crate lazy_static;
