0fae012ef57875dce4469459fc0dbb0f832ec1ee
Evgeni Pandurski <epandurski@gmail.com>
2017-01-02 21:13:23 +0200

    add links to the docs, mention we are on crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,21 @@
 //!
 //! # Usage
 //!
+//! This crate is [on crates.io](https://crates.io/crates/alcibiades)
+//! and can be used by adding `alcibiades` to your dependencies in
+//! your project's `Cargo.toml`.
+//!
+//! ```toml
+//! [dependencies]
+//! alcibiades = "0.2"
+//! ```
+//!
+//! and this to your crate root:
+//!
+//! ```rust
+//! extern crate alcibiades;
+//! ```
+//!
 //! Here is how simple it is to create a chess engine using this crate:
 //!
 //! ```rust,no_run
