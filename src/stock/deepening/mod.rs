c31dae2bc7f62869105d924b760c611059e011bb
Evgeni Pandurski <epandurski@gmail.com>
2017-05-05 22:21:16 +0300

    move thread_executor.rs to mod.rs

--- src/stock/deepening/mod.rs
+++ src/stock/deepening/mod.rs
@@ -3,13 +3,13 @@
 
 mod aspiration;
 mod multipv;
-mod thread_executor;
 
 use self::multipv::Multipv;
-use self::thread_executor::ThreadExecutor;
+use std::thread;
 use std::time::Duration;
+use std::cell::RefCell;
 use std::sync::Arc;
-use std::sync::mpsc::TryRecvError;
+use std::sync::mpsc::{channel, Sender, Receiver, TryRecvError};
 use regex::Regex;
 use uci::{SetOption, OptionDescription};
 use moves::Move;
@@ -179,11 +179,82 @@ impl<T: Search> SetOption for Deepening<T> {
 
 impl<T: Search> Deepening<T> {
     fn search_next_depth(&mut self) {
-        self.multipv.start_search(SearchParams {
-            search_id: 0,
-            depth: self.depth + 1,
-            ..self.params.clone()
-        });
+        self.multipv
+            .start_search(SearchParams {
+                              search_id: 0,
+                              depth: self.depth + 1,
+                              ..self.params.clone()
+                          });
+    }
+}
+
+
+/// A helper type. It turns a `Search` into `SearchExecutor`.
+struct ThreadExecutor<T: Search> {
+    tt: Arc<T::HashTable>,
+    messages_tx: Sender<String>,
+    reports_rx: Receiver<SearchReport<T::ReportData>>,
+    reports_tx: Sender<SearchReport<T::ReportData>>,
+    pending_report: RefCell<Option<SearchReport<T::ReportData>>>,
+    handle: Option<thread::JoinHandle<Value>>,
+}
+
+impl<T: Search> SearchExecutor for ThreadExecutor<T> {
+    type HashTable = T::HashTable;
+
+    type SearchNode = T::SearchNode;
+
+    type ReportData = T::ReportData;
+
+    fn new(tt: Arc<Self::HashTable>) -> Self {
+        let (reports_tx, reports_rx) = channel();
+        Self {
+            tt: tt,
+            messages_tx: channel().0,
+            reports_rx: reports_rx,
+            reports_tx: reports_tx,
+            pending_report: RefCell::new(None),
+            handle: None,
+        }
+    }
+
+    fn start_search(&mut self, params: SearchParams<Self::SearchNode>) {
+        let (messages_tx, messages_rx) = channel();
+        self.messages_tx = messages_tx;
+        self.handle.take().and_then(|h| h.join().ok());
+        self.handle = Some(T::spawn(params,
+                                    self.tt.clone(),
+                                    self.reports_tx.clone(),
+                                    messages_rx));
+    }
+
+    fn wait_report(&self, timeout_after: Duration) {
+        let mut report = self.pending_report.borrow_mut();
+        if report.is_none() {
+            *report = self.reports_rx.recv_timeout(timeout_after).ok();
+        }
+    }
+
+    fn try_recv_report(&mut self) -> Result<SearchReport<Self::ReportData>, TryRecvError> {
+        self.pending_report
+            .borrow_mut()
+            .take()
+            .ok_or(TryRecvError::Empty)
+            .or_else(|_| self.reports_rx.try_recv())
+    }
+
+    fn send_message(&mut self, msg: &str) {
+        self.messages_tx.send(msg.to_string()).ok();
+    }
+}
+
+impl<T: Search> SetOption for ThreadExecutor<T> {
+    fn options() -> Vec<(String, OptionDescription)> {
+        T::options()
+    }
+
+    fn set_option(name: &str, value: &str) {
+        T::set_option(name, value);
     }
 }
 
